70445d883e660b049ccffbf504c3cd900e1a7660
jm1509 <133684712+jm1509@users.noreply.github.com>
2023-06-01 11:00:03 -0500

    Update pong-audio.js
    changed wall sound

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("wall.mp3"); //load sound
+export var wallSound ="C:\Users\mcbra\OneDrive\Desktop\Summer 2023\Videogame\Paddle.mp3.mp3" ("wall.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("paddle.mp3");
